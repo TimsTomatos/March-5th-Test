7fc960df61ddac2b144465d78f470ce57e745fd3
TimsTomatos <imshiny808@gmail.com>
2018-03-05 11:21:55 -1000

    lucky

---
+++ app.js
@@ -0,0 +1,9 @@
+var array = ["html", "javascript", "css", "coding"];
+var numbersOrString = "Coding is fun because";
+array[1].splice
+console.log(array);
+
+//for (var i = Things.length - 1; i >= 0; i--) {
+//	Things[i]
+//}
+
